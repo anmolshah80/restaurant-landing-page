a012c251f923e9e2823875c4f58b3d4fc3e7b545
Anmol Shah <anmolsha80@gmail.com>
2024-09-21 11:37:43 +0545

    features/gallery-and-find-us-section - Create the Gallery and Find Us sections to showcase the photos of meals and opening hours respectively - Create a notes section in README to add links for getting vercel access tokens, orgID and projectID, adding secrets to a github repository, and setup github actions with vercel - Change the deploy_production workflow to only run on commit push to main branch - Create a deploy_preview workflow to generate a preview on each commit push, without deploying to production

--- README.md
+++ README.md
@@ -9,14 +9,14 @@
 - Create a Today's Special section to inform users about the meal that fits their palatte
 - Create the Chef section to let users know about the chef's beliefs
 - Create the Introduction and Laurels sections to showcase the fine dining video, and awards & recognition given to the restaurant respectively
+- Create the Gallery and Find Us sections to showcase the photos of meals and opening hours respectively
 - Add responsiveness to each of these sections for desktop, tablet, and mobile devices
 - Create a `deploy_production.yml` workflow to build and deploy the changes to production in Vercel
+- Create a `deploy_preview.yml` workflow to build and deploy preview in Vercel
 
-## To-dos
+## Notes
 
-- Document the steps to add Secrets to your GitHub project -> https://github.com/anmolshah80/restaurant-landing-page/settings/secrets/actions
-- Document the process to add or get your access token in vercel -> https://vercel.com/account/settings/tokens
-- Document the process to get your project ID in vercel -> https://vercel.com/anmol-shah-projects/modern-restaurant-app/settings/general
-- Document the process to get your org ID in vercel -> https://github.com/orgs/vercel/discussions/3307
-- How can I use GitHub Actions with Vercel (article) -> https://vercel.com/guides/how-can-i-use-github-actions-with-vercel
--
+- To add _`Secrets`_ to your GitHub Repository, the [official doc](https://docs.github.com/en/actions/security-for-github-actions/security-guides/using-secrets-in-github-actions#creating-secrets-for-a-repository) provided by GitHub should suffice
+- Vercel Access Tokens can be created by visiting [Vercel Token Settings](https://vercel.com/account/settings/tokens) page
+- To get your Vercel _`orgID`_ and _`projectID`_, this [GitHub Discussion](https://github.com/orgs/vercel/discussions/3307) can be looked into
+- To setup GitHub Actions with Vercel for both _preview_ and _production_ environment, this official [article](https://vercel.com/guides/how-can-i-use-github-actions-with-vercel) by Vercel can be looked into
--- src/containers/Gallery/Gallery.jsx
+++ src/containers/Gallery/Gallery.jsx
@@ -1,7 +1,75 @@
+import {
+  BsInstagram,
+  BsArrowLeftShort,
+  BsArrowRightShort,
+} from 'react-icons/bs';
+
+import { SubHeading } from '@/components';
+
+import { images } from '@/constants';
+
 import '@/containers/Gallery/Gallery.css';
+import { useRef } from 'react';
+
+const GALLERY_IMAGES = [
+  images.gallery01,
+  images.gallery02,
+  images.gallery03,
+  images.gallery04,
+];
 
 const Gallery = () => {
-  return <div>Gallery</div>;
+  const scrollRef = useRef(null);
+
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if (direction === 'left') {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
+  };
+
+  return (
+    <div className="app__gallery flex__center">
+      <div className="app__gallery-content">
+        <SubHeading title="Instagram" />
+        <h1 className="headtext__cormorant">Photo Gallery</h1>
+        <p className="p__opensans" style={{ color: '#aaa', marginTop: '2rem' }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat
+          mattis ipsum turpis elit elit scelerisque egestas mu.
+        </p>
+        <button type="button" className="custom__button">
+          View More
+        </button>
+      </div>
+
+      <div className="app__gallery-images">
+        <div className="app__gallery-images_container" ref={scrollRef}>
+          {GALLERY_IMAGES.map((image, index) => (
+            <div
+              className="app__gallery-images_card flex__center"
+              key={`gallery image ${index + 1}`}
+            >
+              <img src={image} alt={`gallery-${index + 1}`} />
+              <BsInstagram className="gallery__image-icon" />
+            </div>
+          ))}
+        </div>
+        <div className="app__gallery-images_arrows">
+          <BsArrowLeftShort
+            className="gallery__arrow-icon"
+            onClick={() => scroll('left')}
+          />
+          <BsArrowRightShort
+            className="gallery__arrow-icon"
+            onClick={() => scroll('right')}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Gallery;
