7af51729c100d9fc795fa327ab420532dfbfd751
Anmol Shah <anmolsha80@gmail.com>
2024-09-22 11:43:47 +0545

    Create the Footer section to render an input bar and a button to subscribe to newsletters, contact information, social links, and working hours

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,7 +1,23 @@
+import SubHeading from '@/components/SubHeading/SubHeading';
+
 import '@/components/Footer/Newsletter.css';
 
 const Newsletter = () => {
-  return <div>Newsletter</div>;
+  return (
+    <div className="app__newsletter">
+      <div className="app__newsletter-heading">
+        <SubHeading title="Newsletter" />
+        <h1 className="headtext__cormorant">Subscribe To Our Newsletter</h1>
+        <p className="p__opensans">And never miss latest updates!</p>
+      </div>
+      <div className="app__newsletter-input flex__center">
+        <input type="email" placeholder="Enter your email address" />
+        <button type="button" className="custom__button">
+          Subscribe
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Newsletter;
