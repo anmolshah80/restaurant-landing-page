d9f73b0d434a21c61af4d4b38eec84223f50d944
Anmol Shah <anmolsha80@gmail.com>
2024-09-26 11:11:40 +0545

    Resolve open threads after PR review

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -1,12 +1,10 @@
 import '@/components/Footer/FooterOverlay.css';
 
-const FooterOverlay = () => {
-  return (
-    <div className="app__footerOverlay">
-      <div className="app__footerOverlay-black" />
-      <div className="app__footerOverlay-img app__bg" />
-    </div>
-  );
-};
+const FooterOverlay = () => (
+  <div className="app__footerOverlay">
+    <div className="app__footerOverlay-black" />
+    <div className="app__footerOverlay-img app__bg" />
+  </div>
+);
 
 export default FooterOverlay;
